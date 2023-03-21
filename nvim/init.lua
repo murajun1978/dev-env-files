30e06f9e744320fe64dac0e5c6860263bd853297
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2023-03-22 01:52:58 +0900

    Add nvim

---
+++ nvim/init.lua
@@ -0,0 +1,7 @@
+require("plugins-setup")
+require("core.colorscheme")
+require("core.keymaps")
+require("core.options")
+require("plugins.nvim-tree")
+require("plugins.lualine")
+require("plugins.telescope")
---
+++ nvim/lua/plugins-setup.lua
@@ -0,0 +1,32 @@
+local ensure_packer = function()
+  local fn = vim.fn
+  local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+  if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+    vim.cmd [[packadd packer.nvim]]
+    return true
+  end
+  return false
+end
+
+local packer_bootstrap = ensure_packer()
+
+return require('packer').startup(function(use)
+  use 'wbthomason/packer.nvim'
+  use 'bluz71/vim-nightfly-colors'
+  use 'christoomey/vim-tmux-navigator'
+  use 'szw/vim-maximizer'
+  use 'tpope/vim-surround'
+  use 'nvim-tree/nvim-tree.lua'
+  use 'nvim-treesitter/nvim-treesitter'
+  use 'nvim-tree/nvim-web-devicons'
+  use 'nvim-lua/plenary.nvim'
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
+  use 'nvim-telescope/telescope.nvim'
+  use 'nvim-lualine/lualine.nvim'
+
+  if packer_bootstrap then
+    require('packer').sync()
+  end
+end)
+
