21969d2dd5dd7b45061f0ab1846ed7292694a415
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2024-09-02 13:23:28 +0900

    Add LSP plugins

--- nvim/lua/core/lazy.lua
+++ nvim/lua/core/lazy.lua
@@ -14,7 +14,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({ { import = "plugins" } }, {
+require("lazy").setup({ { import = "plugins" }, { import = "plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
