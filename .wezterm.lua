e00c81312f93f6ab3285897ba631cfc7f125e5b6
murajun1978 <crazy.rendezvous.m.jun@gmail.com>
2024-10-06 13:30:41 +0900

    Add wezterm

---
+++ .wezterm.lua
@@ -0,0 +1,22 @@
+local wezterm = require 'wezterm'
+
+local config = wezterm.config_builder()
+
+config.font = wezterm.font('Hack Nerd Font Mono')
+config.font_size = 13
+
+config.background = {
+  {
+    source = {
+      File = '/Users/murajun1978/Pictures/wallpapers/miko/20240816-2040_a599bd94d1847c98e2b260baded1e5e0.png',
+    },
+    repeat_x = 'Mirror',
+    hsb = {
+      brightness = 0.1
+    },
+    attachment = { Parallax = 0.1 },
+  }
+}
+
+-- and finally, return the configuration to wezterm
+return config
